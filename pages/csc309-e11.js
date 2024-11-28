ef084fb4e4a54852c894aadc2a187437e847cdd5
zfjuliana <144293710+zfjuliana@users.noreply.github.com>
2024-11-28 10:41:49 -0500

    Initial commit

---
+++ pages/csc309-e11.js
@@ -0,0 +1,8 @@
+export default function Response() {
+    return (
+        <div id="auto-tester">
+            Hi. I am zha13020, and I have learned how to develop a full-stack web app
+            and deploy it!
+        </div>
+    );
+}
